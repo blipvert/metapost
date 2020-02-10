d6b24eff4f4ba2c07cd96b8e80c2fc1732d516cf
Craig Lennox <blipvert@blipvertica.com>
2020-02-10 12:48:18 -0500

    removed unnecessary build commands

--- build.sh
+++ build.sh
@@ -164,11 +164,6 @@ fi
 
 ##    --without-system-zlib no, poopler conflicts
 $MAKE
-(cd libs/zlib; $MAKE )
-(cd libs/libpng; $MAKE )
-(cd libs/gmp; $MAKE ; $MAKE gmpbuild )
-(cd libs/mpfr; $MAKE ; $MAKE mpfrbuild )
-(cd texk/web2c; $MAKE $MPOSTEXE )
 
 # go back
 cd ..
